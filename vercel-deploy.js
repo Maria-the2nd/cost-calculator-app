3df8fcf6945d7ecc1e26efe1a516abb9094cdee8
Maria-the2nd <vanquish.ideas@gmail.com>
2025-03-24 18:26:46 +0000

    Add Vercel deployment configurations and scripts

---
+++ vercel-deploy.js
@@ -0,0 +1,7 @@
+// This file is a marker for Vercel to ensure it deploys the latest version
+console.log('Deploying latest version with timestamp:', new Date().toISOString());
+console.log('Version: July 2024 (Rev. 2 - LATEST UPDATE)');
+
+// Make sure environment variables are available
+console.log('Checking environment variables:');
+console.log('NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:', process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ? 'Set' : 'Not set'); 
